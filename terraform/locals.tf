9b070ad128ca035cae23bf67ab5a034bb17421bc
Albert Ribas <albert.ribas@flixbus.com>
2025-01-29 15:30:09 +0100

    Move tf common tags in locals file

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,11 @@
 locals {
   env          = lower(var.env)
   service_name = var.plan_name
+
+  tf_common_tags = {
+    Environment            = local.env
+    Product                = local.service_name
+    Terraform              = "true"
+    "Terraform Repository" = var.repository
+  }
 }
